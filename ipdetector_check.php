4f57e1a18f3eb8dc1cf1512dd40cd422c3536073
xMalware <xmalware2@gmail.com>
2018-06-28 07:48:26 +0200

    Update ipdetector_check.php

--- ipdetector_check.php
+++ ipdetector_check.php
@@ -1,12 +1,12 @@
 <?php
 	
-	/**
-		This PHP file is used to determine if an IP is good or bad (VPN/host/proxy) using the IPDetector API
+	/*
+		This PHP file is used to determine if an IP is good or bad (VPN/host/proxy) by using the IPDetector API
 		You can use the free offer (30 requests per minute) or the paid offer.
 		Create your API key at https://ipdetector.info
-	**/
+	*/
 	
-	function isGoodIp($apiKey, $ip)
+	function isGoodIp($apiKey, $ip, $defaultErrorIpResult = true)
 	{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -16,11 +16,30 @@
 		$result = curl_exec($ch);
 		curl_close($ch);
 	 
+		if ($result == null)
+		{
+			return $defaultErrorIpResult;
+		}
+		
 		$obj = json_decode($result, true);
+		
+		if ($obj == null)
+		{
+			return $defaultErrorIpResult;
+		}
+		
+		if (isset($obj['error']))
+		{
+			// If you want to get the error
+			// or print it, you can uncomment this part
+			//var_dump($obj);
+			return $defaultErrorIpResult;
+		}
+		
 		return $obj['goodIp'] == '1';
 	}
 	
-	$apiKey = "Votre clé API"; // Mettez votre clé API
+	$apiKey = "Your API key"; // Please replace this value by setting your API key
 	$ip = $_SERVER['REMOTE_ADDR']; // IP
 	
 	if (!isGoodIP($apiKey, $ip))
@@ -33,5 +52,5 @@
 		// This IP is good
 		echo 'Good IP';
 	}
-  
+
 ?>
